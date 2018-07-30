a84847a7c1ee37af20f715fe0a00b8089c5d249a
n1ckfg <nick@fox-gieg.com>
2018-07-30 16:16:49 -0400

    add carla classes

---
+++ Source/LidarSim/Private/SensorFactory.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#include "Carla.h"
+#include "Carla/Sensor/SensorFactory.h"
+
+#include "Carla/Actor/ActorBlueprintFunctionLibrary.h"
+#include "Carla/Sensor/SceneCaptureCamera.h"
+
+TArray<FActorDefinition> ASensorFactory::GetDefinitions()
+{
+  FActorDefinition Cameras;
+  bool Success = false;
+  UActorBlueprintFunctionLibrary::MakeCameraDefinition(
+      {TEXT("camera"), ASceneCaptureCamera::StaticClass()},
+      Success,
+      Cameras);
+  check(Success);
+  return {Cameras};
+}
+
+FActorSpawnResult ASensorFactory::SpawnActor(
+    const FTransform &Transform,
+    const FActorDescription &Description)
+{
+  FActorSpawnParameters Params;
+  auto *World = GetWorld();
+  if (World == nullptr)
+  {
+    return {};
+  }
+  auto *Sensor = World->SpawnActorDeferred<ASceneCaptureCamera>(
+      Description.Class,
+      Transform,
+      this,
+      nullptr,
+      ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+  if (Sensor != nullptr)
+  {
+    using ABFL = UActorBlueprintFunctionLibrary;
+    Sensor->SetImageSize(
+        ABFL::RetrieveActorAttributeToInt("image_size_x", Description.Variations, 800),
+        ABFL::RetrieveActorAttributeToInt("image_size_y", Description.Variations, 600));
+    Sensor->SetFOVAngle(
+        ABFL::RetrieveActorAttributeToFloat("fov", Description.Variations, 90.0f));
+    Sensor->SetPostProcessEffect(
+        PostProcessEffect::FromString(
+            ABFL::RetrieveActorAttributeToString("post_processing", Description.Variations, "SceneFinal")));
+  }
+  UGameplayStatics::FinishSpawningActor(Sensor, Transform);
+  return FActorSpawnResult{Sensor};
+}
---
+++ Source/LidarSim/Public/Sensor/SensorFactory.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#pragma once
+
+#include "Carla/Actor/ActorSpawnResult.h"
+#include "Carla/Actor/CarlaActorFactory.h"
+
+#include "SensorFactory.generated.h"
+
+/// Object in charge of spawning sensors.
+UCLASS()
+class CARLA_API ASensorFactory : public ACarlaActorFactory
+{
+  GENERATED_BODY()
+
+  TArray<FActorDefinition> GetDefinitions() final;
+
+  FActorSpawnResult SpawnActor(
+      const FTransform &SpawnAtTransform,
+      const FActorDescription &ActorDescription) final;
+};
